83aaa2183fe27fa6def3400fdbed0af4aba801ad
jinamshah <jinams47@gmail.com>
2022-09-26 23:29:32 -0400

    Added generalizability

--- pruning_VAE.py
+++ pruning_VAE.py
@@ -1,12 +1,33 @@
-from model_VAE import VAE, device, trainer, evaluator
-from torch.optim import Adam
-from nni.compression.pytorch.pruning import L1NormPruner
-from nni.compression.pytorch.speedup import ModelSpeedup
 import torch
+import argparse
+# from model_VAE import VAE, device, trainer, evaluator
+from model_VAE import VAE, trainer, evaluator
+from torch.optim import Adam
+from nni.algorithms.compression.v2.pytorch.pruning import L1NormPruner
+# from nni.compression.pytorch.pruning import L1NormPruner
+from nni.compression.pytorch.speedup import ModelSpeedup
 
-epochs = 15
+
+# epochs = 15
 
 if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description='NNI VAE Example')
+    parser.add_argument('--epochs', type=int, default=15, metavar='N',
+                        help='number of epochs to train (default: 15)')
+    parser.add_argument('--no-cuda', action='store_true', default=False,
+                        help='disables CUDA training')
+    
+    args = parser.parse_args()
+
+    use_cuda = not args.no_cuda and torch.cuda.is_available()
+    epochs = args.epochs
+    pruning_method = args.pruning_method
+
+    if use_cuda:
+        device = torch.device("cuda")
+    else:
+        device = torch.device("cpu")
 
     model = VAE().to(device)
     print("ORIGINAL UN-PRUNED MODEL: \n\n", model)
@@ -45,6 +66,7 @@ if __name__ == '__main__':
     print("\nPRUNED MODEL: \n\n", model)
 
     # Running the pre-training stage with original unpruned model
+    optimizer = Adam(model.parameters(), lr=1e-3)
     for epoch in range(epochs):
         trainer(model, optimizer)
         evaluator(model)
